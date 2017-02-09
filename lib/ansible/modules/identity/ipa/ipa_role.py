030f98fd5015f4e0222eb4ac6e9909429df0e335
Thomas Krahn <thomas.krahn@esailors.de>
2017-02-09 16:15:34 +0100

    ipa_role: Add default vaule to module documentation

--- lib/ansible/modules/identity/ipa/ipa_role.py
+++ lib/ansible/modules/identity/ipa/ipa_role.py
@@ -65,6 +65,7 @@ options:
     - If option is omitted privileges will not be checked or changed.
     - If option is passed all assigned privileges that are not passed will be removed.
     required: false
+    default: None
   service:
     description:
     - List of service names to assign.
